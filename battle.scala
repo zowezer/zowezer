894e6df3383f5de40581ce3d38b5a0dd36b5fe7e
zowezer <m.zowezer@gmail.com>
2020-10-16 15:22:07 +0200

    Update battle.scala

--- battle.scala
+++ battle.scala
@@ -1,86 +1,115 @@
-case BlockWindow{
-    import itroprog.pixelwindow
-
-    val nuberOfBlocks :(Int,Int)
-    val title : String = "Block window"
-    val pixelwindow = new pixelwindow(numberOfBlocks._1 = blockSize, numberOfBlocks._2=blockSize,title)
-    def setBlock(pos:Pos, color: java.awt.color): Unit =
-    def getBlock(pos: Pos): java.awt.color = 
-    de write (
-        text:String
-        pos:Pos
-        color: java.awt.color
-        textSize:Int=blockSize
-        ): Unit = pixelwindow.drawText(
-            text, pos.x=blockSize, pos.y=blockSize,color,textSize
-        )
-    def nextEvent()
-    
-    
-
-
-
-
-
-
-
-}
 package blockbattle
-object Game {
-val windowSize = (30, 50)
-val windowTitle = "EPIC BLOCK BATTLE"
-val blockSize = 14
-val skyRange = 0 to 7
-val grassRange = 8 to 8
-object Color { ??? }
-/** Used with the different ranges and eraseBlocks */
-def backgroundColorAtDepth(y: Int): java.awt.Color = ???
-}
-class Game(
-val leftPlayerName: String = "LEFT",
-val rightPlayerName: String = "RIGHT"
-) {
-import Game._ // direkt tillgång till namn på medlemmar i kompanjon
-val window = new BlockWindow(windowSize, windowTitle, blockSize)
-val leftMole: Mole = ???
-val rightMole: Mole = ???
-def drawWorld(): Unit = ???
-/** Use to erase old points, e.g updated score */
-def eraseBlocks(x1: Int, y1: Int, x2: Int, y2: Int): Unit = ???
-def update(mole: Mole): Unit = ??? // update, draw new, erase old
-def gameLoop(): Unit = ???
-def start(): Unit = {
-println("Start digging!")
-println(s"$leftPlayerName ${leftMole.keyControl}")
-println(s"$rightPlayerName ${rightMole.keyControl}")
-drawWorld()
-gameLoop()
-}
+case class Pos(x: Int, y: Int) {
+    def moved(delta: (Int, Int)): Pos = new Pos(x+delta._1, y+delta._2)}
 
-var quit = false
-val delayMillis = 80
-def gameLoop(): Unit = {
-while(!quit) {
-val t0 = System.currentTimeMillis
-handleEvents() // ändrar riktning vid tangenttryck etc.
-update(leftMole) // flyttar, ritar, suddar, etc.
-update(rightMole)
-val elapsedMillis = (System.currentTimeMillis - t0).toInt
-Thread.sleep((delayMillis - elapsedMillis) max 0)
-}
 
-def handleEvents(): Unit = {
-var e = window.nextEvent()
-while (e != BlockWindow.Event.Undefined) {
-e match {
-case BlockWindow.Event.KeyPressed(key) =>
-??? // ändra riktning på resp. mullvad
-case BlockWindow.Event.WindowClosed =>
-??? // avsluta spel-loopen
+case class KeyControl(left: String, right: String, up: String, down: String){
+  def direction(key: String): (Int, Int) = key match{
+    case `left` => (-1,0)
+    case `right` => (1,0)
+    case `up` => (0,-1)
+    case `down` => (0,1)
+    case _ => (0,0)
+  }
+  def has(key: String): Boolean = key match {
+    case `left` => true
+    case `right` => true
+    case `up` => true
+    case `down` => true
+    case _ => false
+   }
+  }
+
+
+
+
+
+
+
+
+
+
+
+class Mole(
+    val name: String,
+    var pos: Pos,
+    var dir: (Int, Int),
+    val color: java.awt.Color,
+    val keyControl: KeyControl)
+    {
+    var points = 0
+    override def toString =s"Mole[name=$name, pos=$pos, dir=$dir, points=$points]"/**Om keyControl.has(key) så uppdateras riktningen dir enligt keyControl*/
+    
+    def setDir(key: String): Unit = {
+      if (keyControl.has(key))  dir = keyControl.direction(key)
+      }
+    
+    def reverseDir(): Unit = dir = (dir._1*-1,dir._2*-1)
+    def move(): Unit = pos = nextPos
+    def nextPos: Pos = pos.moved(dir)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ class BlockWindow(
+   val nbrOfBlocks: (Int, Int),
+   val title: String = "BLOCK WINDOW",
+   val blockSize: Int = 14) {
+     import introprog.PixelWindow 
+     val pixelWindow =new PixelWindow(nbrOfBlocks._1*blockSize, nbrOfBlocks._2*blockSize, title)
+     def setBlock(pos: Pos, color: java.awt.Color): Unit = ???
+     def getBlock(pos: Pos): java.awt.Color = ???
+     def write(
+       text: String,
+       pos: Pos,
+       color: java.awt.Color,
+       textSize: Int = blockSize): Unit =
+       pixelWindow.drawText(text, pos.x*blockSize, pos.y*blockSize, color, textSize)
+     def nextEvent(maxWaitMillis: Int = 10): BlockWindow.Event.EventType  = {
+       importBlockWindow.Event._
+       pixelWindow.awaitEvent(maxWaitMillis)
+       pixelWindow.lastEventType match{
+         case PixelWindow.Event.KeyPressed => KeyPressed(pixelWindow.lastKey)
+         case PixelWindow.Event.WindowClosed=>WindowClosed
+         case _  => Undefined
+         }
+      }
+  }
+object BlockWindow {
+  def delay(millis: Int): Unit = Thread.sleep(millis)
+object Event {
+  trait EventType
+   case classKeyPressed(key: String) extends EventType 
+   case object WindowClosed          extends EventType 
+   case object Undefined             extends EventType
+   }
 }
-e = window.nextEvent()
-})
-}
-window.setBlock(mole.nextPos, mole.color) // draw new
-window.setBlock(mole.pos, Color.tunnel) // erase old
-mole.move() // update
